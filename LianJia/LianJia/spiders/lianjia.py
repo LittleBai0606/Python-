4803960f83ea3d2ec660fd3223227d0db0478c74
BenWhite <958255724@qq.com>
2017-10-16 18:38:53 +0800

    对链家广州成交房爬虫的初始化

--- LianJia/LianJia/spiders/lianjia.py
+++ LianJia/LianJia/spiders/lianjia.py
@@ -1,11 +1,34 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+import requests
+import time
+from LianJia.items import LianjiaItem
+from lxml import etree
+from scrapy_redis.spiders import RedisSpider
 
 
 class LianjiaSpider(scrapy.Spider):
     name = 'lianjia'
-    allowed_domains = ['lianjia.com']
-    start_urls = ['http://lianjia.com/']
+    allowed_domains = ['gz.lianjia.com']
+    start_urls = []
+    regions = {
+        'tianhe' : '天河',
+        'yuexiu' : '越秀',
+        'liwan' : '荔湾',
+        'haizhu' : '海珠',
+        'panyu' : '番禺',
+        'baiyun' : '白云',
+        'huangpugz' : '黄埔',
+        'conghua' : '从化',
+        'zengcheng' : '增城',
+        'huadou' : '花都',
+        'nansha' : '南沙'
+    }
+
+    for region in list(regions.keys()):
+        start_urls.append("https://gz.lianjia.com/chengjiao/" + region + "/")
+
 
     def parse(self, response):
         pass
